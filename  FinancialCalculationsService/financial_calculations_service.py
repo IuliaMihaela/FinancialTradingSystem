aa0a9b4dc41132aed2cb407f72fda6a49e0c367b
IuliaMihaela <20IMC10290@fh-krems.ac.at>
2022-06-06 19:11:05 +0300

    organizing calculations service

---  FinancialCalculationsService/financial_calculations_service.py
+++  FinancialCalculationsService/financial_calculations_service.py
@@ -21,7 +21,6 @@ time_series = create_time_series(100, 300)
 while True:
     # getting the messsage from the jobs queue
     r = get('http://localhost:7500/message_jobs/api', json={'username': 'felicity', 'token': token})
-    # job_assets = input(": ")
     try:
         job_response = json.loads(r.content.decode())
         job_assets = job_response["message_body"]["assets"]  # getting the assets from the response as a string
@@ -36,11 +35,13 @@ while True:
             ]
         else:
             tasks = None
-
+        # Scatter parameters arrays
         unit = comm.scatter(tasks, root=0)
+
         p = json.loads(unit)
         print(f'[{comm.rank}]: parameters {p}')
         calc = predict_value(linear_fit(time_series[p['parameter1']]), 301)
+
         # gather results
         result = comm.gather(calc, root=0)
         if comm.rank == 0:
@@ -58,58 +59,6 @@ while True:
 
     except:
         break
-
-
-
-########################################  testing 1
-# comm = MPI.COMM_WORLD
-# time_series = create_time_series(100, 300)
-# scatter_tasks = None
-#
-# list_assets = [10, 17, 23, 56, 60]
-#
-# if comm.rank == 0:
-#     tasks = [
-#         json.dumps({'parameter1': list_assets[0]}),
-#         json.dumps({'parameter1': list_assets[1]}),
-#         json.dumps({'parameter1': list_assets[2]}),
-#         json.dumps({'parameter1': list_assets[3]}),
-#         json.dumps({'parameter1': list_assets[4]})
-#     ]
-#     scatter_tasks = [None] * comm.size
-#     current_proc = 0
-#     for task in tasks:
-#         if scatter_tasks[current_proc] is None:
-#             scatter_tasks[current_proc] = []
-#         scatter_tasks[current_proc].append(task)
-#         current_proc = (current_proc + 1) % comm.size
-#
-# else:
-#     tasks = None
-#
-# units = comm.scatter(tasks, root=0)
-# calcs = []
-#
-# if units is not None:
-#     for unit in units:
-#         p = json.loads(unit)
-#         print(f'[{comm.rank}]: parameters {p}')
-#
-#         calc = predict_value(linear_fit(time_series[p['parameter1']]), 301)
-#
-#         calc = [calc, comm.rank]
-#         calcs.append(calc)
-#
-#
-# # gather results
-# result = comm.gather(calcs, root=0)
-#
-# if comm.rank == 0:
-#     print("[root]: Result is ", result)
-#     avg_result = sum(result) / len(result)
-#     print(f"Avg Result is: {avg_result}")
-#
-#
 
 
 
@@ -142,7 +91,7 @@ while True:
 #     avg_result = sum(result) / len(result)
 #     print(f"Avg Result is: {avg_result}")
 #
-#
+
 
 
 
